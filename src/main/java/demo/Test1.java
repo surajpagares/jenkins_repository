38338c11f3b8b78c34d13d27c69302bd2bb44f21
suraj <suraj@Suraj_Pagare>
2023-01-09 16:13:53 +0530

    Jenkis first commit

---
+++ src/main/java/demo/Test1.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void m1()
+{
+	Reporter.log("Hello",true);
+}
+}
---
+++ src/main/java/demo/Test2.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void m2()
+	{
+		Reporter.log("Hii",true);
+	}
+	}
