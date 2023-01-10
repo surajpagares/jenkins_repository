aaf49003609705c11727b78167b365bf99d87c76
suraj <suraj@Suraj_Pagare>
2023-01-10 15:34:12 +0530

    Second commit

--- src/main/java/demo/Test1.java
+++ src/main/java/demo/Test1.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.testng.annotations.Test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
--- src/main/java/demo/Test3.java
+++ src/main/java/demo/Test3.java
@@ -4,10 +4,10 @@ import org.testng.annotations.Test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test2 {
-	@Test
-	public void m2()
-	{
-		Reporter.log("Hii",true);
-	}
-	}
+public class Test3 {
+@Test
+public void m3()
+{
+	Reporter.log("Hello!!! Hi!!!!",true);
+}
+}
